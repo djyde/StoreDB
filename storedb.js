49b4245173236be0f0fbeef235b33f3d9a6cf369
Ivan <dangelzm@gmail.com>
2015-09-10 12:10:23 +0300

    Add AMD/CommonJS/Global wrapper

--- storedb.js
+++ storedb.js
@@ -1,100 +1,107 @@
-var storedb = function(collectionName){
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+    typeof define === 'function' && define.amd ? define(factory) :
+      global.storedb = factory()
+}(this, function () {
+  'use strict';
+  return function (collectionName) {
     collectionName = collectionName ? collectionName : 'default';
-    
+
     var err;
     var cache = localStorage[collectionName] ? JSON.parse(localStorage[collectionName]) : [];
 
     return {
 
-        insert: function(obj,callback){
-            obj["_id"] = new Date().valueOf();          
-            cache.push(obj);
-            localStorage.setItem(collectionName,JSON.stringify(cache));
-            if(callback)
-                callback(err,obj);
-        },
+      insert: function (obj, callback) {
+        obj["_id"] = new Date().valueOf();
+        cache.push(obj);
+        localStorage.setItem(collectionName, JSON.stringify(cache));
+        if (callback)
+          callback(err, obj);
+      },
 
-        find: function(obj, callback){
-            if(arguments.length == 0){
-                return cache;
-            } else {
-                var result = [];
+      find: function (obj, callback) {
+        if (arguments.length == 0) {
+          return cache;
+        } else {
+          var result = [];
 
-                for(var key in obj){
-                    for(var i = 0; i < cache.length; i++){
-                        if(cache[i][key] == obj[key]){
-                            result.push(cache[i]);
-                        }
-                    }
-                }
-                if(callback)
-                    callback(err,result);
-                else
-                    return result;
-            }     
-        },
+          for (var key in obj) {
+            for (var i = 0; i < cache.length; i++) {
+              if (cache[i][key] == obj[key]) {
+                result.push(cache[i]);
+              }
+            }
+          }
+          if (callback)
+            callback(err, result);
+          else
+            return result;
+        }
+      },
 
-        update: function(obj,upsert,callback){
+      update: function (obj, upsert, callback) {
 
-            for(var key in obj){
-                for(var i = 0; i < cache.length; i++){
-                    if(cache[i][key] == obj[key]){
+        for (var key in obj) {
+          for (var i = 0; i < cache.length; i++) {
+            if (cache[i][key] == obj[key]) {
 
-                        end_loops:
-                        for(var upsrt in upsert){
-                            switch(upsrt){
-                            case "$inc":
-                                for(var newkey in upsert[upsrt]){
-                                    cache[i][newkey] = parseInt(cache[i][newkey]) + parseInt(upsert[upsrt][newkey]);
-                                }
-                                break;
+              end_loops:
+                for (var upsrt in upsert) {
+                  switch (upsrt) {
+                    case "$inc":
+                      for (var newkey in upsert[upsrt]) {
+                        cache[i][newkey] = parseInt(cache[i][newkey]) + parseInt(upsert[upsrt][newkey]);
+                      }
+                      break;
 
-                            case "$set":
-                                for(var newkey in upsert[upsrt]){
-                                    cache[i][newkey] = upsert[upsrt][newkey];
-                                }
-                                break;
+                    case "$set":
+                      for (var newkey in upsert[upsrt]) {
+                        cache[i][newkey] = upsert[upsrt][newkey];
+                      }
+                      break;
 
-                            case "$push":
-                                for(var newkey in upsert[upsrt]){
-                                    cache[i][newkey].push(upsert[upsrt][newkey]);
-                                }
-                                break;
+                    case "$push":
+                      for (var newkey in upsert[upsrt]) {
+                        cache[i][newkey].push(upsert[upsrt][newkey]);
+                      }
+                      break;
 
-                            default:
-                                upsert['_id'] = cache[i]['_id'];
-                                cache[i] = upsert;
-                                break end_loops;
-                            }
-                        }
-                    }
+                    default:
+                      upsert['_id'] = cache[i]['_id'];
+                      cache[i] = upsert;
+                      break end_loops;
+                  }
                 }
             }
-            localStorage.setItem(collectionName,JSON.stringify(cache));
-            if(callback)
-                callback(err);
+          }
+        }
+        localStorage.setItem(collectionName, JSON.stringify(cache));
+        if (callback)
+          callback(err);
 
-        },
+      },
 
-        remove: function(obj,callback){
-            if(arguments.length == 0){
-                localStorage.removeItem(collectionName);
-            } else {
+      remove: function (obj, callback) {
+        if (arguments.length == 0) {
+          localStorage.removeItem(collectionName);
+        } else {
 
-                for(var key in obj){
-                    for (var i = cache.length - 1; i >= 0; i--) {
-                        if(cache[i][key] == obj[key]){
-                            cache.splice(i,1);
-                        }
-                    }
-                }
-                localStorage.setItem(collectionName, JSON.stringify(cache));
+          for (var key in obj) {
+            for (var i = cache.length - 1; i >= 0; i--) {
+              if (cache[i][key] == obj[key]) {
+                cache.splice(i, 1);
+              }
             }
-            
-            if(callback)
-                callback(err);
-            
+          }
+          localStorage.setItem(collectionName, JSON.stringify(cache));
         }
 
+        if (callback)
+          callback(err);
+
+      }
+
     };
-};
+  };
+}));
